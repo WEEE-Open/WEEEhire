afeba532ae0005e08eeeb1cd7eb86fa13fa57ae8
Hyd3L <hydel@linuxmail.org>
2018-09-07 22:08:14 +0200

    Matricola-based authentication

--- weeehire/controllers/signup.py
+++ weeehire/controllers/signup.py
@@ -2,7 +2,7 @@
 """Signup controller module"""
 
 from datetime import datetime
-from tg import expose, redirect, validate, flash, lurl, request
+from tg import expose, redirect, validate, flash, lurl, request, abort
 from weeehire.lib.base import BaseController
 from weeehire.model import DBSession, User
 from tw2.core import RegexValidator, EmailValidator, Required, _
@@ -16,31 +16,24 @@ def generate_password():
     alphabet = ascii_letters + digits
     password = ""
     for c in range(32):
-        password += str(alphabet[randint(0, len(alphabet)-1)])
+        password += str(alphabet[randint(0, len(alphabet) - 1)])
     return password
 
 
-def is_polito_mail(email: str):
-    if not email:
+def is_valid_sn(sn: str):
+    print(len(sn))
+    if len(sn) != 6:
         return False
-    if '@' not in email:
-        return False
-    if not email.split('@')[1]:
-        return False
-    if email.split('@')[1] != 'studenti.polito.it':
-        return False
-    if email.startswith('s') or email.startswith('S'):
-        for i in range(1, 7):
-            if not email[i].isdigit():
-                return False
-    else:
-        return False
+    for i in range(len(sn)):
+        if not sn[i].isdigit():
+            return False
     return True
 
 
 class SignupForm(twf.Form):
     class child(twf.widgets.BaseLayout):
-        email = twf.TextField(css_class="form-control mt-10")
+        sn = twf.TextField(css_class="form-control mt-10")
+
     action = lurl('/signup/verify')
     submit = twf.SubmitButton(value='Submit', css_class="btn btn-success mt-10")
 
@@ -48,32 +41,34 @@ class SignupForm(twf.Form):
 class SignupController(BaseController):
     # Uncomment this line if your controller requires an authenticated user
     # allow_only = predicates.not_anonymous()
-    
+
     @expose('weeehire.templates.signup')
     def index(self, status=None, **kw):
-        if status == 'badmail':
-            flash(_('Please, insert your polito email address [sXXXXXX@studenti.polito.it]'), 'error')
+        if status == 'badsn':
+            flash(_('Caro utonto, inserisci il numero di matricola (senza la S davanti)'), 'error')
         if status == 'login':
-            flash(_('This email is already registered, please login'), 'error')
+            flash(_('Questa mail è già in uso, fai il login'), 'error')
         if status == 'success':
-            flash(_('Check your email inbox and click on the activation link'))
+            flash(_('Apri il link di conferma che hai ricevuto sulla tua mail del poli'))
         return dict(page='signup-index', form=SignupForm)
 
     @expose()
     def verify(self, **kw):
-        email = kw['email']
-        if is_polito_mail(email):
+        if len(kw) < 1:
+            abort(404)
+        matricola = kw['sn']
+        if is_valid_sn(matricola):
+            email = 's' + matricola + '@studenti.polito.it'
             user = User.by_email_address(email)
             if not user:
                 confirm_link = 'http://127.0.0.1:8080/signup/register?email=' + email
                 token = generate_password()
                 confirm_link += '&auth=' + token
                 password = generate_password()
-                serial = email.split("@")[0]
 
                 u = User()
-                u.user_name = serial
-                u.display_name = serial
+                u.user_name = 's' + matricola
+                u.display_name = 's' + matricola
                 u.email_address = email
                 u.token = token
                 u.password = password
@@ -85,30 +80,34 @@ class SignupController(BaseController):
                                   sender="weeeopen@yandex.ru",
                                   recipients=[email],
                                   body=("Ciawa! asd\nClicca qua per attivare l'account " + confirm_link +
-                                       "\nIl tuo username: " + serial +
-                                       "\nLa tua password: " + password + "   (se la perdi sono cazzi tuoi asd)" +
-                                       "\nCiawa di nuovo")
+                                        "\n\n------------------CREDENZIALI-DI-ACCESSO------------------"
+                                        "\nIl tuo username: s" + matricola +
+                                        "\nLa tua password: " + password + "   (se la perdi sono cazzi tuoi asd)" +
+                                        "\n----------------------------------------------------------"
+                                        "\n\nCiawa di nuovo! asd")
                                   )
                 mailer.send(message)
             else:
                 return redirect("/signup?status=login")
             return redirect("/signup?status=success")
         else:
-            return redirect("/signup?status=badmail")
+            return redirect("/signup?status=badsn")
 
     @expose('weeehire.templates.post_signup')
     def register(self, email, auth, **kw):
+        if not email or not auth:
+            abort(404)
         user = User.by_email_address(email)
         if user:
             if user.created:
-                flash(_('User already verified'), 'warning')
+                flash(_('Utente già verificato'), 'warning')
                 return dict()
             if user.token == auth:
                 user.created = datetime.now()
-                flash(_('Authentication successful'))
+                flash(_('Autenticazione riuscita'))
             else:
-                flash(_('Authentication failed'), 'error')
+                flash(_('Autenticazione fallita'), 'error')
         else:
-            flash(_('User not found'), 'error')
+            flash(_('Utente non registrato'), 'error')
         DBSession.flush()
         return dict()
