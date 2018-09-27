3cce157517c7ac77dc08e7a198044760434f5545
Hyd3L <hydel@linuxmail.org>
2018-09-27 15:30:01 +0200

    Closes #56 - Fixed status_link in mail template

--- weeehire/controllers/form.py
+++ weeehire/controllers/form.py
@@ -144,13 +144,13 @@ class FormController(BaseController):
 
 Abbiamo ricevuto la tua candidatura per il team WEEE Open, questa è la pagina da cui potrai verificare lo stato della tua domanda:
 
-${status_link}
+%s
 
 Se la domanda sarà approvata, riceverai un'email sempre a questo indirizzo con scritto chi contattare per passare al colloquio. Le stesse informazioni saranno visibili anche alla pagina di cui sopra.
 
 Buona fortuna ;)
 Il software WEEEHire per conto del team WEEE Open
-"""
+""" % status_link
                                 )
                           )
         mailer.send(message)
